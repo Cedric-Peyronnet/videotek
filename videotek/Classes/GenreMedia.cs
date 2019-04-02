7b3c70edd3236516514a6c3c2c77055f852f7b40
Tony <tony.cannard.apg@gmail.com>
2019-04-02 10:19:59 +0200

    Ajout Genre et GenreMedia

---
+++ videotek/Classes/GenreMedia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videotek.Classes
+{
+    class GenreMedia
+    {
+        private int idGenre { get; set; }
+        private int idMedia { get; set; }
+    }
+}
