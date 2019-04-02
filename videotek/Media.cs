a3d63d2d713258d298adf598209cc4696be0cd7f
Tony <tony.cannard.apg@gmail.com>
2019-04-02 09:35:38 +0200

    Ajout Media

---
+++ videotek/Media.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videotek
+{
+    class Media
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private int id { get; set; }
+        private TimeSpan duree { get; set; }
+        private DateTime dateSortie { get; set; }
+        private string titre { get; set; }
+        private string description { get; set; }
+        private bool vu { get; set; }
+        private int note { get; set; }
+        private string commentaire { get; set; }
+        private TypeMedia type { get; set; }
+        private int ageMinimum { get; set; }
+        private bool supportPhysique { get; set; }
+        private bool supportNumerique { get; set; }
+        private string image { get; set; }
+        private Langue langueVO { get; set; }
+        private Langue langueMedia { get; set; }
+        private Langue sousTitre { get; set; }
+    }
+}
